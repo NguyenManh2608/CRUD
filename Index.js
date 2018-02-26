ee44f376ebae3cb2ea2afe57c12e2fc5eeb3653f
NguyenManh <manhnv@sphinx-software.com>
2018-02-26 12:03:43 +0700

    update Index.js

--- Index.js
+++ Index.js
@@ -1,7 +1,14 @@
 let express = require('express');
+
 let mysql = require('mysql');
+
 let app = express();
+
 let bodyParser = require('body-parser');
+
+let middleware = require('./middleware/middleware');
+
+let port = process.env.PORT || 8000;
 
 let con = mysql.createConnection({
     host      : 'localhost',
@@ -11,15 +18,15 @@ let con = mysql.createConnection({
 });
 
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
+app.get('/get/', function(req, res) {
     con.query('select * from Books',function (err, result) {
         if(err) throw err;
         res.json(result);
     });
 });
-
 
 app.get('/book/:id', function (req, res) {
     con.query('select * from Books WHERE id =' + req.params.id, function (err, result) {
@@ -29,21 +36,18 @@ app.get('/book/:id', function (req, res) {
 });
 
 app.put('/:id',function (req, res) {
-    con.query('Update Books SET title = "Truyen ngan" WHERE id = ' +req.params.id, function (err, result) {
+    con.query('Update Books SET ? WHERE id = ' +req.params.id, req.body, function (err, result) {
         if (err) throw err;
         res.json(result);
     });
 });
 
-app.post('/Books', (req, res) => {
+app.post('/post', middleware, function(req, res) {
     con.query('insert into Books set ?', req.body, function(err) {
         if(err) throw err;
-
         res.send('Success...!');
     });
-
 });
-
 
 app.delete('/delete/:id',function (req,res) {
     con.query('Delete from Books Where id ='+ req.params.id , function (err) {
@@ -51,4 +55,7 @@ app.delete('/delete/:id',function (req,res) {
         res.send("Success....!");
     });
 });
-app.listen(8080);
+
+app.listen(port, function () {
+    console.log(" You access success on port " + port);
+});
