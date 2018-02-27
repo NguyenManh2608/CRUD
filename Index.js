dca73e7f35db17ae9b89fc37fd8444061f1d8109
NguyenManh <manhnv@sphinx-software.com>
2018-02-27 14:16:02 +0700

    add limit length of text and soft Delete

--- Index.js
+++ Index.js
@@ -1,14 +1,12 @@
 let express = require('express');
-
 let mysql = require('mysql');
-
+let bodyParser = require('body-parser');
+let mdw = require('./middleware/mdtitle');
 let app = express();
 
-let bodyParser = require('body-parser');
-
-let middleware = require('./middleware/middleware');
-
 let port = process.env.PORT || 8000;
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 let con = mysql.createConnection({
     host      : 'localhost',
@@ -17,42 +15,39 @@ let con = mysql.createConnection({
     database  : 'CRUDBook'
 });
 
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.use(bodyParser.json());
-
-app.get('/get/', function(req, res) {
-    con.query('select * from Books',function (err, result) {
+app.get('/books/get', function(req, res) {
+    con.query('select * from Books WHERE isDelete = 0',function (err, result) {
         if(err) throw err;
         res.json(result);
     });
 });
 
-app.get('/book/:id', function (req, res) {
-    con.query('select * from Books WHERE id =' + req.params.id, function (err, result) {
-        if(err) throw err;
+app.get('/book/get/:id', function (req, res) {
+    con.query('select * from Books WHERE id = ? limit 1' ,req.params.id, function (err, result) {
+        if(err)  res.status(404).send({ message : " Not Found this file"});
         res.json(result);
     });
 });
 
-app.put('/:id',function (req, res) {
-    con.query('Update Books SET ? WHERE id = ' +req.params.id, req.body, function (err, result) {
+
+app.put('/book/:id',mdw.checkAuthortLength,mdw.checkIt,mdw.checkTitleLength ,function (req, res) {
+    con.query('UPDATE Books SET ? WHERE id = '+ req.params.id, req.body, function (err) {
         if (err) throw err;
-        res.json(result);
+        res.status(200).send({ message : "Update  id = "+ req.params.id + " Success"});
     });
 });
 
-app.post('/post', middleware, function(req, res) {
-    con.query('insert into Books set ?', req.body, function(err) {
+app.post('/book/post',mdw.checkAuthortLength,mdw.checkIt,mdw.checkTitleLength , function(req, res) {
+    con.query('INSERT INTO Books SET ?', req.body, function(err) {
         if(err) throw err;
-        res.send('Success...!');
+        res.status(201).send({ message : "Create a data success"})
     });
 });
 
-app.delete('/delete/:id',function (req,res) {
-    con.query('Delete from Books Where id ='+ req.params.id , function (err) {
+app.delete('/book/delete/:id',function (req,res) {
+    con.query('UPDATE Books SET isDelete = 1 WHERE id ='+ req.params.id , function (err) {
         if(err) throw err;
-        res.send("Success....!");
+        res.status(200).send({ message : "You've been deleted !!!"});
     });
 });
 
